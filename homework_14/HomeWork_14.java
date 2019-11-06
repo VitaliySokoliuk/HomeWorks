75af12d2186a16128ebb40a5adab2bc217cff474
Vitaliy Sokoliuk <Vit.sokol0@gmail.com>
2019-11-06 17:17:03 +0200

    'Override_task_1'

--- homework_14/HomeWork_14.java
+++ homework_14/HomeWork_14.java
@@ -1,26 +1,27 @@
 package homework_14;
-
 import java.util.Scanner;
 
 public class HomeWork_14 {
 
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         String str = sc.next();
-
-        if (str.length() == 5) {
-            if (str.toUpperCase().charAt(0) == str.toUpperCase().charAt(4)
-                    && str.toUpperCase().charAt(1) == str.toUpperCase().charAt(3)) {
-                System.out.println("Слово паліндром");
-            } else {
-                System.out.println("Слово не паліндром");
+        char[] st = str.toCharArray();
+        
+        boolean f = true;
+        if (st.length == 5) {
+            for(int i = 0; i < str.length(); i++){
+                if(st[i] != st[st.length - i - 1]){
+                    System.out.println("Слово не паліндром");
+                    f = false;
+                    break;
+                }
             }
+            if(f)
+                System.out.println("Слово паліндром");
         } else {
             System.out.println("Слово не з 5 букв");
         }
-        
     }
-
 }
