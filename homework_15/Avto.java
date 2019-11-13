2f6042374874fa2f37f301a35f2cef1a40df4352
Vitaliy Sokoliuk <Vit.sokol0@gmail.com>
2019-11-13 14:59:50 +0200

    'update_JavaCore_11'

--- homework_15/Avto.java
+++ homework_15/Avto.java
@@ -1,5 +1,7 @@
 package homework_15;
 
+import java.util.Comparator;
+
 public class Avto {
 
     private int kks;
@@ -10,22 +12,42 @@ public class Avto {
     Avto(int year) {
         this.year = year;
     }
-    
+
     Avto() {
         this.kks = (int) (100 + Math.random() * 200);
         this.year = (int) (2000 + Math.random() * 20);
         this.kermo = new Kermo();
         this.dvugyn = new Dvugyn();
     }
-    
-    public void setYear(int year){
+
+    public void setYear(int year) {
         this.year = year;
     }
-    
+
+    public Integer getYear() {
+        return year;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return "Кінських сил: " + kks + ", рік: " + year + ", матеріал керма: " + kermo.getMaterial()
                 + ", діаметр керма: " + kermo.getDiameter() + ", кількість циліндрів: " + dvugyn.getChulindres() + "\n";
     }
+}
+
+class Sort implements Comparator<Avto> {
+
+    public int compare(Avto a, Avto b) {
+        return a.getYear() - b.getYear();
+    }
 
 }
+
+class Sort2 implements Comparator<Avto> {
+    
+        public int compare(Avto a, Avto b) {
+            return b.getYear() - a.getYear();
+        }
+        
+    }
+
--- homework_15/Task_2.java
+++ homework_15/Task_2.java
@@ -1,34 +1,59 @@
 package homework_15;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Task_2 {
-    
+
     static Scanner scan;
-    
+
     private static void menu() {
         System.out.println("Зроби вибір: ");
         System.out.println("\t1.Вивести дані елементів. ");
-        System.out.println("\t2.Для всіх елементів встановити значення рік. ");
+        System.out.println("\t2.Для всіх елементів встановити значення первинного об'єкта. ");
         System.out.println("\t3.Сортування за спаданням. ");
         System.out.println("\t4.Сортування за зростанням. ");
         System.out.println("Введи \"quit\" щоб вийти. ");
     }
-    
+
     private static void task_1(Avto[][] avto) {
         System.out.println(Arrays.deepToString(avto));
     }
-    
+
     private static void task_2(Avto[][] avto) {
-        Avto av = new Avto(2005);
-        Arrays.fill(avto, av);
+        Avto avt = new Avto();
+        System.out.println("\nПервинний об'єкт: " + avt.toString());
+        for (int j = 0; j < avto.length; j++) {
+            for (int i = 0; i < avto[j].length; i++) {
+                Arrays.fill(avto[i], avt);
+            }
+        }
+
         System.out.println(Arrays.deepToString(avto));
     }
     
+    private static void task_3(Avto[][] avto) {
+        for (int j = 0; j < avto.length; j++) {
+            for (int i = 0; i < avto[j].length; i++) {
+                Arrays.sort(avto[i], new Sort2());
+            }
+        }
+        System.out.println(Arrays.deepToString(avto));
+    }
+    
+    private static void task_4(Avto[][] avto) {
+        for (int j = 0; j < avto.length; j++) {
+            for (int i = 0; i < avto[j].length; i++) {
+                Arrays.sort(avto[i], new Sort());
+            }
+        }
+        System.out.println(Arrays.deepToString(avto));
+    }
+
     public static void main(String[] args) {
 
-        int m = (int) (1 + Math.random() * 4);
-        int n = (int) (1 + Math.random() * 4);
+        int m = (int) (3 + Math.random() * 4);
+        int n = (int) (3 + Math.random() * 4);
         System.out.println("m - " + m + ", n - " + n);
         Avto[][] avto1 = new Avto[m][n];
         for (int i = 0; i < m; i++) {
@@ -36,9 +61,9 @@ public class Task_2 {
                 avto1[i][j] = new Avto();
             }
         }
-        
+
         System.out.println(Arrays.deepToString(avto1));
-        
+
         scan = new Scanner(System.in);
         menu();
         String line = scan.next();
@@ -50,17 +75,16 @@ public class Task_2 {
                 case "2":
                     task_2(avto1);
                     break;
-                /*case "3":
-                    task_3();
+                case "3":
+                    task_3(avto1);
                     break;
                 case "4":
-                    task_4();
-                    break;*/
+                    task_4(avto1);
+                    break;
             }
             menu();
             line = scan.next();
         }
-        
-        
+
     }
 }
